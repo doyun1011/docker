cb17ebf2dc3bfbbce987f355f17b55e804164ea9
ryum <ow1011@naver.com>
2024-03-04 21:43:48 +0900

    signup

--- src/main/java/milktea/taro/controller/MemberController.java
+++ src/main/java/milktea/taro/controller/MemberController.java
@@ -1,13 +1,17 @@
 package milktea.taro.controller;
 
+import milktea.taro.entity.Member;
 import milktea.taro.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 
 @RestController
-@RequestMapping
+
 public class MemberController {
 
     private final MemberService memberService;
@@ -15,5 +19,17 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/")
+    public String getSignUpForm(Model model) {
+        model.addAttribute("member", new Member());
+        return "signup_form"; // 회원가입 폼을 나타내는 HTML 페이지의 이름
+    }
+
+    @PostMapping("/signup")
+    public String signUp(@ModelAttribute Member member) {
+        memberService.signUp(member.getUsername(), member.getPassword());
+        return "redirect:/"; // 루트 경로로 리다이렉션
     }
 }
--- src/main/java/milktea/taro/service/MemberService.java
+++ src/main/java/milktea/taro/service/MemberService.java
@@ -1,5 +1,6 @@
 package milktea.taro.service;
 
+import milktea.taro.entity.Member;
 import milktea.taro.repository.MemberRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -13,5 +14,12 @@ public class MemberService {
     @Autowired
     public MemberService(MemberRepository memberRepository) {
         this.memberRepository = memberRepository;
+    }
+
+    public void signUp(String username, String password) {
+        Member member = new Member();
+        member.setUsername(username);
+        member.setPassword(password);
+        memberRepository.save(member);
     }
 }
