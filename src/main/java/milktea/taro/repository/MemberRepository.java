9e26b2e2219f2244e6a11ad89f816464fc6c4346
ryum <ow1011@naver.com>
2024-03-04 23:08:31 +0900

    test

--- src/main/java/milktea/taro/repository/MemberRepository.java
+++ src/main/java/milktea/taro/repository/MemberRepository.java
@@ -4,5 +4,5 @@ import milktea.taro.entity.Member;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
-    // 추가적인 메서드가 필요한 경우 여기에 작성
+
 }
