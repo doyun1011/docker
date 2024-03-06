ee2fbf4d099259f7707adb5b5c73717ee8fe3998
ryum <ow1011@naver.com>
2024-03-06 09:04:47 +0900

    rds change

--- src/main/java/milktea/taro/entity/Member.java
+++ src/main/java/milktea/taro/entity/Member.java
@@ -1,5 +1,6 @@
 package milktea.taro.entity;
 
+import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -12,6 +13,7 @@ import lombok.Setter;
 @Getter
 @Setter
 @AllArgsConstructor
+@Entity
 public class Member {
 
 
